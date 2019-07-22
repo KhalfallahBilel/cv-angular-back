bc3cfbea4a6b1dd7f4a5a34f092e3bf62314a4ff
khalfallah.bilel@gmail.com <khalfallah.bilel@gmail.com>
2019-07-22 14:57:15 +0200

    ok

---
+++ src/main/java/com/bilelkhalfallah/cv/model/User.java
@@ -0,0 +1,104 @@
+package com.bilelkhalfallah.cv.model;
+
+import com.bilelkhalfallah.cv.helper.Auditable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "USER")
+@EntityListeners(AuditingEntityListener.class)
+public class User extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "userName")
+    private String userName;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "authdata")
+    private String authdata;
+
+    public User(Date createdDate, Date lastModifiedDate, long id, String userName, String firstName, String lastName, String password, String authdata) {
+        super(createdDate, lastModifiedDate);
+        this.id = id;
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.authdata = authdata;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAuthdata() {
+        return authdata;
+    }
+
+    public void setAuthdata(String authdata) {
+        this.authdata = authdata;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", password='" + password + '\'' +
+                ", authdata='" + authdata + '\'' +
+                ", createdDate=" + createdDate +
+                ", lastModifiedDate=" + lastModifiedDate +
+                '}';
+    }
+}
