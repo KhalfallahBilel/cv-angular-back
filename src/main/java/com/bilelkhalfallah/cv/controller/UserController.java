e9dc45e22a42c6fcdd1656f11f248a95fda6039e
khalfallah.bilel@gmail.com <khalfallah.bilel@gmail.com>
2019-07-22 15:47:56 +0200

    ok

---
+++ src/main/java/com/bilelkhalfallah/cv/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.bilelkhalfallah.cv.controller;
+
+
+import com.bilelkhalfallah.cv.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class UserController {
+
+    public UserController() {
+    }
+
+    @Autowired
+    IUserRepository iUserRepository;
+
+    
+}
